8270e645a74c0b2d1c3f3678eb56672656316dfc
nikhitha <madanikhitha752@gmail.com>
2024-11-26 15:12:07 +0530

    Odd.java with content madanikhitha

---
+++ Odd.java
@@ -0,0 +1,29 @@
+public class OddNumbers {
+
+public static void main(String[] args) {
+
+Scanner scanner = new Scanner(System.in);
+
+System.out.print("Enter the limit :");
+
+int limit = scanner.nextInt();
+
+System.out.println("Odd numbers
+
+from 1 to "limit + ":");
+
+for (int i = 1; i <= limit; i++) {
+
+if (i % 2 != 0) {
+
+System.out.print(i + " ");
+
+}
+
+}
+
+scanner.close();
+
+}
+
+}
